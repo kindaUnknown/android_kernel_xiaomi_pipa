28343ff0e8571d8ecc433be28dba8044b30415e6
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-24 07:22:55 -0700

    simple_lmk: Introduce Simple Low Memory Killer for Android
    This is a complete low memory killer solution for Android that is small
    and simple. Processes are killed according to the priorities that
    Android gives them, so that the least important processes are always
    killed first. Processes are killed until memory deficits are satisfied,
    as observed from kswapd struggling to free up pages. Simple LMK stops
    killing processes when kswapd finally goes back to sleep.
    
    The only tunables are the desired amount of memory to be freed per
    reclaim event and desired frequency of reclaim events. Simple LMK tries
    to free at least the desired amount of memory per reclaim and waits
    until all of its victims' memory is freed before proceeding to kill more
    processes.
    
    Change-Id: I94be1bd84c8454bf2a135ee48c93f1342d5e188b
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -99,6 +99,7 @@
 #endif
 #include <linux/devfreq_boost.h>
 #include <linux/scs.h>
+#include <linux/simple_lmk.h>
 
 #include <linux/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1048,6 +1049,7 @@ static inline void __mmput(struct mm_struct *mm)
 	}
 	if (mm->binfmt)
 		module_put(mm->binfmt->module);
+	simple_lmk_mm_freed(mm);
 	mmdrop(mm);
 }
 
