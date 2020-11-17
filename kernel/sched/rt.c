ed3af44a703b714f5ac8d3e77c21ee7c26f26d82
Peter Zijlstra <peterz@infradead.org>
2020-11-17 18:19:32 -0500

    sched: Introduce sched_class::pick_task()
    Because sched_class::pick_next_task() also implies
    sched_class::set_next_task() (and possibly put_prev_task() and
    newidle_balance) it is not state invariant. This makes it unsuitable
    for remote task selection.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    [Vineeth: folded fixes]
    Signed-off-by: Vineeth Remanan Pillai <viremana@linux.microsoft.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Don Hiatt <dhiatt@digitalocean.com>
    Tested-by: Hongyu Ning <hongyu.ning@linux.intel.com>
    Tested-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20210422123308.437092775@infradead.org

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1804,7 +1804,7 @@ static struct task_struct *_pick_next_task_rt(struct rq *rq)
 	return rt_task_of(rt_se);
 }
 
-static struct task_struct *pick_next_task_rt(struct rq *rq)
+static struct task_struct *pick_task_rt(struct rq *rq)
 {
 	struct task_struct *p;
 
@@ -1812,7 +1812,17 @@ static struct task_struct *pick_next_task_rt(struct rq *rq)
 		return NULL;
 
 	p = _pick_next_task_rt(rq);
-	set_next_task_rt(rq, p);
+
+	return p;
+}
+
+static struct task_struct *pick_next_task_rt(struct rq *rq)
+{
+	struct task_struct *p = pick_task_rt(rq);
+
+	if (p)
+		set_next_task_rt(rq, p);
+
 	return p;
 }
 
@@ -2754,6 +2764,7 @@ const struct sched_class rt_sched_class = {
 
 #ifdef CONFIG_SMP
 	.balance		= balance_rt,
+	.pick_task		= pick_task_rt,
 	.select_task_rq		= select_task_rq_rt,
 	.set_cpus_allowed       = set_cpus_allowed_common,
 	.rq_online              = rq_online_rt,
