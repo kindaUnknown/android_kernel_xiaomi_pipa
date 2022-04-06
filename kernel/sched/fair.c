1196250178da86c619a825d357c5eedc85487da2
zgpeng <zgpeng.linux@gmail.com>
2022-04-06 17:57:05 +0800

    sched/fair: Move calculate of avg_load to a better location
    In calculate_imbalance function, when the value of local->avg_load is
    greater than or equal to busiest->avg_load, the calculated sds->avg_load is
    not used. So this calculation can be placed in a more appropriate position.
    
    Signed-off-by: zgpeng <zgpeng@tencent.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Samuel Liao <samuelliao@tencent.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/1649239025-10010-1-git-send-email-zgpeng@tencent.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10269,9 +10269,6 @@ static inline void calculate_imbalance(struct lb_env *env, struct sd_lb_stats *s
 			return;
 		}
 
-		sds->avg_load = (sds->total_load * SCHED_CAPACITY_SCALE) /
-				sds->total_capacity;
-
 		/*
 		 * If the local group is more loaded than the average system
 		 * load, don't try to pull any tasks.
@@ -10281,6 +10278,8 @@ static inline void calculate_imbalance(struct lb_env *env, struct sd_lb_stats *s
 			return;
 		}
 
+		sds->avg_load = (sds->total_load * SCHED_CAPACITY_SCALE) /
+				sds->total_capacity;
 	}
 
 	/*
