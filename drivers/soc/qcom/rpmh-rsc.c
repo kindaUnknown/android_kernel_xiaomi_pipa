9ce413150dd5477048c45e27b5da9236fb329108
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-10-23 19:38:26 +0530

    kernel: Implement xiaomi longpress oops mechanism
    Change-Id: If900d098edbb7edc24771bbf0ae0b3c5e4b30ff0
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/soc/qcom/rpmh-rsc.c
+++ drivers/soc/qcom/rpmh-rsc.c
@@ -22,6 +22,7 @@
 
 #include <soc/qcom/cmd-db.h>
 #include <soc/qcom/tcs.h>
+
 #include <dt-bindings/soc/qcom,rpmh-rsc.h>
 
 #include "rpmh-internal.h"
@@ -72,6 +73,18 @@
 
 #define ACCL_TYPE(addr)			((addr >> 16) & 0xF)
 #define NR_ACCL_TYPES			3
+
+#define rpmh_spin_lock(lock)				\
+do {	\
+	if (!oops_in_progress)\
+		spin_lock(lock);	\
+} while (0)
+
+#define rpmh_spin_unlock(lock)				\
+do {	\
+	if (!oops_in_progress)\
+		spin_unlock(lock);	\
+} while (0)
 
 static const char * const accl_str[] = {
 	"", "", "", "CLK", "VREG", "BUS",
@@ -128,7 +141,7 @@ static int tcs_invalidate(struct rsc_drv *drv, int type)
 
 	tcs = get_tcs_of_type(drv, type);
 
-	spin_lock(&drv->lock);
+	rpmh_spin_lock(&drv->lock);
 	if (bitmap_empty(tcs->slots, MAX_TCS_SLOTS))
 		goto done;
 
@@ -143,7 +156,7 @@ static int tcs_invalidate(struct rsc_drv *drv, int type)
 	bitmap_zero(tcs->slots, MAX_TCS_SLOTS);
 
 done:
-	spin_unlock(&drv->lock);
+	rpmh_spin_unlock(&drv->lock);
 	return ret;
 }
 
@@ -407,7 +420,8 @@ static int tcs_write(struct rsc_drv *drv, const struct tcs_request *msg)
 	if (IS_ERR(tcs))
 		return PTR_ERR(tcs);
 
-	spin_lock(&drv->lock);
+	rpmh_spin_lock(&drv->lock);
+
 	if (msg->state == RPMH_ACTIVE_ONLY_STATE && drv->in_solver_mode) {
 		ret = -EINVAL;
 		goto done_write;
@@ -436,7 +450,7 @@ static int tcs_write(struct rsc_drv *drv, const struct tcs_request *msg)
 	__tcs_trigger(drv, tcs_id, true);
 
 done_write:
-	spin_unlock(&drv->lock);
+	rpmh_spin_unlock(&drv->lock);
 	return ret;
 }
 
@@ -450,9 +464,11 @@ done_write:
  * Return: 0 on success, -EINVAL on error.
  * Note: This call blocks until a valid data is written to the TCS.
  */
+ extern int in_long_press;
 int rpmh_rsc_send_data(struct rsc_drv *drv, const struct tcs_request *msg)
 {
 	int ret;
+	int count = 0;
 
 	if (!msg || !msg->cmds || !msg->num_cmds ||
 	    msg->num_cmds > MAX_RPMH_PAYLOAD) {
@@ -466,7 +482,13 @@ int rpmh_rsc_send_data(struct rsc_drv *drv, const struct tcs_request *msg)
 			pr_debug("DRV:%s TCS Busy, retrying RPMH message send: addr=%#x\n",
 					    drv->name, msg->cmds[0].addr);
 			udelay(10);
+			count++;
 		}
+		if ((count == 50000) && (in_long_press)) {
+			printk(KERN_ERR "Long Press :TCS Busy but log saved!");
+			break;
+		}
+
 	} while (ret == -EBUSY);
 
 	return ret;
@@ -540,12 +562,12 @@ static int tcs_ctrl_write(struct rsc_drv *drv, const struct tcs_request *msg)
 	if (IS_ERR(tcs))
 		return PTR_ERR(tcs);
 
-	spin_lock(&drv->lock);
+	rpmh_spin_lock(&drv->lock);
 	/* find the TCS id and the command in the TCS to write to */
 	ret = find_slots(tcs, msg, &tcs_id, &cmd_id);
 	if (!ret)
 		__tcs_buffer_write(drv, tcs_id, cmd_id, msg);
-	spin_unlock(&drv->lock);
+	rpmh_spin_unlock(&drv->lock);
 
 	return ret;
 }
