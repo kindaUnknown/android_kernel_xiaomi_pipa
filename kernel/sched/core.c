a3ccdecc8a1fc3f33c8c869b5d9229101fd0d1eb
Peter Zijlstra <peterz@infradead.org>
2024-08-14 00:25:55 +0200

    sched: Rework dl_server
    When a task is selected through a dl_server, it will have p->dl_server
    set, such that it can account runtime to the dl_server, see
    update_curr_task().
    
    Currently p->dl_server is set in pick*task() whenever it goes through
    the dl_server, clearing it is a bit of a mess though. The trivial
    solution is clearing it on the final put (now that we have this
    location).
    
    However, this gives a problem when:
    
    	p = pick_task(rq);
    	if (p)
    		put_prev_set_next_task(rq, prev, next);
    
    picks the same task but through a different path, notably when it goes
    from picking through the dl_server to a direct pick or vice-versa. In
    that case we cannot readily determine wether we should clear or
    preserve p->dl_server.
    
    An additional complication is pick_*task() setting p->dl_server for a
    remote pick, it might still need to update runtime before it schedules
    the core_pick.
    
    Close all these holes and remove all the random clearing of
    p->dl_server by:
    
     - having pick_*task() manage rq->dl_server
    
     - having the final put_prev_task() clear p->dl_server
    
     - having the first set_next_task() set p->dl_server = rq->dl_server
    
     - complicate the core_sched code to save/restore rq->dl_server where
       appropriate.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20240813224016.259853414@infradead.org

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3251,8 +3251,6 @@ static void ttwu_do_wakeup(struct rq *rq, struct task_struct *p, int wake_flags,
 		rq->idle_stamp = 0;
 	}
 #endif
-
-	p->dl_server = NULL;
 }
 
 static void
@@ -5251,6 +5249,8 @@ __pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 	const struct sched_class *class;
 	struct task_struct *p;
 
+	rq->dl_server = NULL;
+
 	/*
 	 * Optimization: we know that if all tasks are in the fair class we can
 	 * call that function directly, but only if the @prev task wasn't of a
@@ -5271,20 +5271,6 @@ __pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 			put_prev_set_next_task(rq, prev, p);
 		}
 
-		/*
-		 * This is a normal CFS pick, but the previous could be a DL pick.
-		 * Clear it as previous is no longer picked.
-		 */
-		if (prev->dl_server)
-			prev->dl_server = NULL;
-
-		/*
-		 * This is the fast path; it cannot be a DL server pick;
-		 * therefore even if @p == @prev, ->dl_server must be NULL.
-		 */
-		if (p->dl_server)
-			p->dl_server = NULL;
-
 		return p;
 	}
 
@@ -5303,14 +5289,6 @@ restart:
 			break;
 	}
 #endif
-
-	/*
-	 * We've updated @prev and no longer need the server link, clear it.
-	 * Must be done before ->pick_next_task() because that can (re)set
-	 * ->dl_server.
-	 */
-	if (prev->dl_server)
-		prev->dl_server = NULL;
 
 	for_each_class(class) {
 		if (class->pick_next_task) {
