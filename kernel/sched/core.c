9aa92e206c50c9702033220a0c9d565ed9f54ecf
Peter Zijlstra <peterz@infradead.org>
2024-08-14 00:25:54 +0200

    sched: Combine the last put_prev_task() and the first set_next_task()
    Ensure the last put_prev_task() and the first set_next_task() always
    go together.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20240813224016.158454756@infradead.org

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5268,8 +5268,7 @@ __pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		/* Assume the next prioritized class is idle_sched_class */
 		if (!p) {
 			p = pick_task_idle(rq);
-			put_prev_task(rq, prev);
-			set_next_task_first(rq, p);
+			put_prev_set_next_task(rq, prev, p);
 		}
 
 		/*
@@ -5321,8 +5320,7 @@ restart:
 		} else {
 			p = class->pick_task(rq);
 			if (p) {
-				put_prev_task(rq, prev);
-				set_next_task_first(rq, p);
+				put_prev_set_next_task(rq, prev, p);
 				return p;
 			}
 		}
