22bb43428d91a4180449b8b93732f01e8eda6471
Rudi Setiyawan <diphons@gmail.com>
2024-08-22 12:54:09 +0700

    Import Oplus Feature
    - oproject
    - oext4
    - nandswap
    - uxio first
    - wakelock profile
    - sleep monitor
    - nwpower
    
    Change-Id: I61b74ee5f22d0903517697bee8195d9944c01f25
    Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -157,6 +157,10 @@ static unsigned long dirty_bytes_min = 2 * PAGE_SIZE;
 
 /* this is needed for the proc_dointvec_minmax for [fs_]overflow UID and GID */
 static int maxolduid = 65535;
+#ifdef CONFIG_OPLUS_FEATURE_UXIO_FIRST
+unsigned int sysctl_uxio_io_opt = true;
+bool sysctl_wbt_enable = true;
+#endif
 static int minolduid;
 
 static int ngroups_max = NGROUPS_MAX;
@@ -444,6 +448,22 @@ static struct ctl_table kern_table[] = {
 		.maxlen         = sizeof(unsigned int),
 		.mode           = 0644,
 		.proc_handler   = proc_dointvec,
+	},
+#endif
+#ifdef CONFIG_OPLUS_FEATURE_UXIO_FIRST
+	{
+			.procname	= "uxio_first_opt",
+			.data		= &sysctl_uxio_io_opt,
+			.maxlen		= sizeof(int),
+			.mode		= 0644,
+			.proc_handler	= proc_dointvec,
+	},
+	{
+			.procname	= "wbt_enable",
+			.data		= &sysctl_wbt_enable,
+			.maxlen		= sizeof(int),
+			.mode		= 0644,
+			.proc_handler	= proc_dointvec,
 	},
 #endif
 #ifdef CONFIG_SCHED_DEBUG
