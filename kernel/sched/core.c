3346e399cf5650c2e8c6e6a3dc49003c9d933eb1
Rudi Setiyawan <diphons@gmail.com>
2024-09-30 18:07:46 +0700

    sched/core: Cleanup unsed functions Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1565,9 +1565,6 @@ static inline void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 
 void activate_task(struct rq *rq, struct task_struct *p, int flags)
 {
-	if (task_contributes_to_load(p))
-		rq->nr_uninterruptible--;
-
 	enqueue_task(rq, p, flags);
 }
 
