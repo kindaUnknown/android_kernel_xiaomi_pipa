bb3c4762355fdfb9909c54a2cc627c008b1ab9bb
mrsrimar22 <mrsrimar22@gmail.com>
2024-11-07 16:37:02 +0700

    scsi: ufs: Squash revert product changes
    At high utilization, clock scaling has an important role in
    partial file reading, instead of improving stability and efficiency,
    disabling clock scaling creates new problems on I/O when doing memory stress.
    
    Also revert HIGHPRIO and single release due to race conditions
    with vfs_path_lookup and also issues with e2fsck
    
    Squash revert of:
      - 5bfed396fa3e ("scsi: ufs: Use WQ_HIGHPRI for gating work.")
      - be79be2fef85 ("scsi: ufs: disable clock scaling")
      - f8f45f660ece ("scsi: ufs: atomic update for clkgating_enable")
      - 29fb462c5d0f ("scsi: ufs: call single_release to avoid memory leak")
      - 1d8adc37a82e ("scsi: ufs: Disable interrupt in reset path")
    
    Change-Id: Ib7606ea5530ff9b025901a42379affae43387029
    Signed-off-by: mrsrimar22 <mrsrimar22@gmail.com>

--- drivers/scsi/ufs/ufs-debugfs.c
+++ drivers/scsi/ufs/ufs-debugfs.c
@@ -391,7 +391,6 @@ static const struct file_operations ufsdbg_err_inj_scenario_ops = {
 	.open		= ufsdbg_err_inj_scenario_open,
 	.read		= seq_read,
 	.write		= ufsdbg_err_inj_scenario_write,
-	.release        = single_release,
 };
 
 static int ufsdbg_err_inj_stats_read(struct seq_file *file, void *data)
@@ -433,7 +432,6 @@ static const struct file_operations ufsdbg_err_inj_stats_ops = {
 	.open		= ufsdbg_err_inj_stats_open,
 	.read		= seq_read,
 	.write		= ufsdbg_err_inj_stats_write,
-	.release        = single_release,
 };
 
 static void ufsdbg_setup_fault_injection(struct ufs_hba *hba)
@@ -617,7 +615,6 @@ static const struct file_operations ufsdbg_tag_stats_fops = {
 	.open		= ufsdbg_tag_stats_open,
 	.read		= seq_read,
 	.write		= ufsdbg_tag_stats_write,
-	.release        = single_release,
 };
 
 static int ufsdbg_query_stats_show(struct seq_file *file, void *data)
@@ -689,7 +686,6 @@ static const struct file_operations ufsdbg_query_stats_fops = {
 	.open		= ufsdbg_query_stats_open,
 	.read		= seq_read,
 	.write		= ufsdbg_query_stats_write,
-	.release        = single_release,
 };
 
 static int ufsdbg_err_stats_show(struct seq_file *file, void *data)
@@ -794,7 +790,6 @@ static const struct file_operations ufsdbg_err_stats_fops = {
 	.open		= ufsdbg_err_stats_open,
 	.read		= seq_read,
 	.write		= ufsdbg_err_stats_write,
-	.release        = single_release,
 };
 
 static int ufshcd_init_statistics(struct ufs_hba *hba)
@@ -874,7 +869,6 @@ static int ufsdbg_host_regs_open(struct inode *inode, struct file *file)
 static const struct file_operations ufsdbg_host_regs_fops = {
 	.open		= ufsdbg_host_regs_open,
 	.read		= seq_read,
-	.release        = single_release,
 };
 
 static int ufsdbg_dump_health_desc_show(struct seq_file *file, void *data)
@@ -1115,7 +1109,6 @@ static int ufsdbg_show_hba_open(struct inode *inode, struct file *file)
 static const struct file_operations ufsdbg_show_hba_fops = {
 	.open		= ufsdbg_show_hba_open,
 	.read		= seq_read,
-	.release	= single_release,
 };
 
 static int ufsdbg_dump_device_desc_open(struct inode *inode, struct file *file)
@@ -1127,7 +1120,6 @@ static int ufsdbg_dump_device_desc_open(struct inode *inode, struct file *file)
 static const struct file_operations ufsdbg_dump_device_desc = {
 	.open		= ufsdbg_dump_device_desc_open,
 	.read		= seq_read,
-	.release	= single_release,
 };
 
 static int ufsdbg_string_desc_serial_open(struct inode *inode, struct file *file)
@@ -1438,7 +1430,6 @@ static const struct file_operations ufsdbg_power_mode_desc = {
 	.open		= ufsdbg_power_mode_open,
 	.read		= seq_read,
 	.write		= ufsdbg_power_mode_write,
-	.release	= single_release,
 };
 
 static int ufsdbg_dme_read(void *data, u64 *attr_val, bool peer)
@@ -1618,7 +1609,6 @@ static const struct file_operations ufsdbg_req_stats_desc = {
 	.open		= ufsdbg_req_stats_open,
 	.read		= seq_read,
 	.write		= ufsdbg_req_stats_write,
-	.release        = single_release,
 };
 
 static int ufsdbg_clear_err_state(void *data, u64 val)
