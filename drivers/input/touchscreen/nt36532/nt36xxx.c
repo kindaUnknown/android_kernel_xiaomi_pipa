28b1015eac9717dfa16f59beb023e7c2bd0abbb2
CuriousNom <aryanbiswas7585@gmail.com>
2025-10-25 23:08:23 +0530

    drivers/input/touchscreen/nt36532: Remove unused panel_list variable
    Change-Id: If7e3397fbc01c14d9622a66c74fe156f1b007c85

--- drivers/input/touchscreen/nt36532/nt36xxx.c
+++ drivers/input/touchscreen/nt36532/nt36xxx.c
@@ -1432,7 +1432,6 @@ static int nvt_get_panel_type(struct nvt_ts_data *ts_data)
 	int i;
 	int j;
 	u8 *lockdown = ts_data->lockdown_info;
-	struct nvt_config_info *panel_list = ts->config_array;
 
 	for (j = 0; j < 60; j++) {
 		if (lockdown[1] == 0x42) {
