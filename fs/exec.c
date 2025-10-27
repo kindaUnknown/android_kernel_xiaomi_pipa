e3ce38616695e0f5c1ae33661d56e1d97010fc9f
CuriousNom <aryanbiswas7585@gmail.com>
2025-10-27 08:32:53 +0530

    Revert "fs/exec: remove camera affinity from perf cores"
    This reverts commit 25efac6ba82608813e1937e0fc962f3d0533f52d.

--- fs/exec.c
+++ fs/exec.c
@@ -115,6 +115,7 @@ static DEFINE_RWLOCK(binfmt_lock);
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/vendor.qti.hardware.display.composer-service"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
+#define CAMERA "com.android.camera"
 #define SYSTEMUI "com.android.systemui"
 static struct task_struct *zygote32_task;
 static struct task_struct *zygote64_task;
@@ -1937,6 +1938,11 @@ static int __do_execve_file(int fd, struct filename *filename,
 		else if (unlikely(!strncmp(filename->name,
 					   SURFACEFLINGER_BIN,
 					   strlen(SURFACEFLINGER_BIN)))) {
+			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		} else if (unlikely(!strncmp(filename->name,
+					   CAMERA,
+					   strlen(CAMERA)))) {
 			current->flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		} else if (unlikely(!strncmp(filename->name,
