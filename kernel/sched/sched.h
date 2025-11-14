a063b6d121349d5f5259b7f9d4d2f3544159307e
CuriousNom <aryanbiswas7585@gmail.com>
2025-11-14 08:11:01 +0530

    !fixup: sched/fair: Fix per-CPU kthread and wakee stacking for asym CPU capacity
    * Import missing declaration.
    
    Signed-off-by: CuriousNom <aryanbiswas7585@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3097,3 +3097,16 @@ static inline bool sched_energy_enabled(void) { return false; }
 #endif
 
 extern u64 avg_vruntime(struct cfs_rq *cfs_rq);
+
+#ifdef CONFIG_SMP
+static inline bool is_per_cpu_kthread(struct task_struct *p)
+{
+	if (!(p->flags & PF_KTHREAD))
+		return false;
+
+	if (p->nr_cpus_allowed != 1)
+		return false;
+
+	return true;
+}
+#endif
